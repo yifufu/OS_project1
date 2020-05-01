0f205315c5bd1e185380bc53137e8b8f0c8c984e
yifufu <yifu.arlj@gmail.com>
2020-05-01 14:02:56 +0800

    forget to put main code

--- my_printk.c
+++
@@ -1,8 +0,0 @@
-#include <linux/linkage.h>
-#include <linux/kernel.h>
-
-asmlinkage int sys_my_printk(int pid, long t_start_sec, long t_start_nsec, long t_end_sec, long t_end_nsec)
-{
-	printk("[Project1] %d %ld.%ld %ld.%ld\n", pid, t_start_sec, t_start_nsec, t_end_sec, t_end_nsec);
-	return 0;
-}
